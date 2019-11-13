3f882b95dea5a07558b6f3520f483665212860bd
Marina Guseva <marina.guseva@contentful.com>
2019-11-13 14:20:47 +0100

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'China is awesome!',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '0123456789',
+    category:      'chinese'
+  },
+  {
+    name:         'France is awesome!',
+    address:      'Paris 12 Ave Blanc',
+    phone_number:  '+70123456789',
+    category:      'french'
+  },
+  {
+    name:         'Belgium is awesome!',
+    address:      'Brussels Street 1/13',
+    phone_number:  '+90123456789',
+    category:      'belgian'
+  },
+  {
+    name:         'Japan is awesome!',
+    address:      'Tokio Hotel 113',
+    phone_number:  '+8970123456789',
+    category:      'japanese'
+  },
+  {
+    name:         'Italy is awesome!',
+    address:      'Roma Mama, 120',
+    phone_number:  '122350123456789',
+    category:      'italian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
