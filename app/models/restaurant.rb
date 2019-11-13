fd864f81b06622cf9f949f3bbb472de4369c2f1a
Marina Guseva <marina.guseva@contentful.com>
2019-11-13 14:14:09 +0100

    Add models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
+COUSINES = %w[chinese italian japanese french belgian]
+
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: COUSINES }
 end
